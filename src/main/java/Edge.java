5c345a81f2edf69fda3e6956e5a0bd9fc8ef5a6e
Artthegor <jeremy.thiebaud@outlook.fr>
2019-12-11 16:43:05 +0100

    add javadoc to Edge.java file

--- src/main/java/Edge.java
+++ src/main/java/Edge.java
@@ -19,11 +19,28 @@ class Edge implements Comparable<Edge>{
     private int weight;
     private boolean isWeighted;
 
+    /**
+     * <b> Constuctor Edge</b>
+     *
+     * Creates and edge between the two nodes in parameters
+     *
+     * @param nodeFrom node the edge comes from
+     * @param nodeTo node the edge goes to
+     */
     public Edge(Node nodeFrom, Node nodeTo){
         this(nodeFrom, nodeTo, 0);
         this.isWeighted = false;
     }
 
+    /**
+     * <b> Constuctor Edge with weight</b>
+     *
+     * Creates and edge between the two nodes in parameters, with the weight in parameters
+     *
+     * @param nodeFrom node the edge comes from
+     * @param nodeTo node the edge goes to
+     * @param weight weight of the edge
+     */
     public Edge(Node nodeFrom, Node nodeTo, int weight){
         this.nodeFrom = nodeFrom;
         this.nodeTo = nodeTo;
@@ -31,11 +48,24 @@ class Edge implements Comparable<Edge>{
         this.isWeighted = true;
     }
 
-
+    /**
+     * <b> Function to print the edge</b>
+     *
+     * Return the string reprsenting the edge
+     *
+     * @return the string reprsenting the edge
+     */
     public String printEdge(){
         return toString();
     }
 
+    /**
+     * <b> Function to print the edge</b>
+     *
+     * Return the string reprsenting the edge
+     *
+     * @return the string reprsenting the edge
+     */
     public String toString(){
         if (!isWeighted) {
             return nodeFrom.getNumber() + " -> " + nodeTo.getNumber();
@@ -44,19 +74,50 @@ class Edge implements Comparable<Edge>{
         return nodeFrom.getNumber() + " -> " + nodeTo.getNumber() + "[label=\"" + weight + "\"]";
     }
 
+    /**
+     * <b> Getter the start node of the edge</b>
+     *
+     * Return the start node of the edge
+     *
+     * @return the start node of the edge
+     */
     public Node getNodeFrom(){
         return nodeFrom;
     }
 
+
+    /**
+     * <b> Getter the end node of the edge</b>
+     *
+     * Return the end node of the edge
+     *
+     * @return the end node of the edge
+     */
     public Node getNodeTo(){
         return nodeTo;
     }
 
+
+    /**
+     * <b> Getter the weight of the edge</b>
+     *
+     * Return the weight of the edge
+     *
+     * @return the the weight of the edge
+     */
     public int getWeight() {
         return weight;
     }
 
-
+    /**
+     * <b>Function compareTo</b>
+     *
+     * Used to compare the current edge with the edge in parameter
+     *
+     * @param edge the edge to compare with the current edge
+     *
+     * @return 0 if edges are similar, 1 or -1 if they are different
+     */
     @Override
     public int compareTo(Edge edge) {
 
@@ -79,6 +140,15 @@ class Edge implements Comparable<Edge>{
         return 1;
     }
 
+    /**
+     * <b>Function equals</b>
+     *
+     * Compare two edges
+     *
+     * @param o Object which is an edge in this case
+     *
+     * @return boolean
+     */
     @Override
     public boolean equals(Object o) {
         if (o == null) {
@@ -95,6 +165,13 @@ class Edge implements Comparable<Edge>{
                 edge.getNodeFrom() == this.getNodeFrom();
     }
 
+    /**
+     * <b>Function hashCode</b>
+     *
+     * Hash the edge
+     *
+     * @return hash code which represent the edge
+     */
     @Override
     public int hashCode() {
         return Objects.hash(nodeFrom, nodeTo, weight);
